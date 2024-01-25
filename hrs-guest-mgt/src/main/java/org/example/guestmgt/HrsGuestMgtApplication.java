e366ea984bc0e0082ca2c46b94f54a2c1f04ccad
sjf65n <kewenhan@gmail.com>
2024-01-25 13:03:19 +0800

    feat(*): Project init

---
+++ hrs-guest-mgt/src/main/java/org/example/guestmgt/HrsGuestMgtApplication.java
@@ -0,0 +1,13 @@
+package org.example.guestmgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrsGuestMgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrsGuestMgtApplication.class, args);
+	}
+
+}
---
+++ hrs-parcel-mgt/src/main/java/org/example/parcelmg/HrsParcelMgtApplication.java
@@ -0,0 +1,12 @@
+package org.example.parcelmg;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrsParcelMgtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrsParcelMgtApplication.class, args);
+    }
+
+}
